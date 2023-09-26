74ddc22d6cc17cb5700d334d75c33b4ad91329a0
danimtnz <danimtnz2004@gmail.com>
2023-09-26 16:39:26 +0200

    Navigation (no con go_router)

--- lib/config/menu/menu_items.dart
+++ lib/config/menu/menu_items.dart
@@ -20,7 +20,7 @@ const appMenuItems = <MenuItem>[
   MenuItem(
     title: 'Tarjetas',
     subTitle: 'Un contenedor estilizado',
-    link: '/card',
+    link: '/cards',
     icon: Icons.credit_card
   ),
 ];
