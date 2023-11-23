9c544f68dc3a99e3d9dbfc257f373731337e17f1
danimtnz <danimtnz2004@gmail.com>
2023-11-24 00:51:06 +0100

    InfiniteScroll - Inicio

--- lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
+++ lib/presentation/screens/infinite_scroll/infinite_scroll_screen.dart
@@ -1,11 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-
-class InfiniteScrollScreen extends StatelessWidget {
+class InfiniteScrollScreen extends StatefulWidget {
   const InfiniteScrollScreen({super.key});
   static const name = 'infinite_screen';
+
+  @override
+  State<InfiniteScrollScreen> createState() => _InfiniteScrollScreenState();
+}
+
+class _InfiniteScrollScreenState extends State<InfiniteScrollScreen> {
+
+  List<int> imagesIds = [1,2,3,4,5];
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.black,
+      // appBar: AppBar(
+      //   title: const Text('InfiniteScroll'),
+      // ),
+      body: 
+      MediaQuery.removePadding(
+        context: context,
+        removeTop: true,
+        child: ListView.builder(
+          itemCount: imagesIds.length,
+          itemBuilder: (context, index) {
+            return FadeInImage(
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: 300,
+              placeholder: const AssetImage('assets/images/jar-loading.gif'),
+              image: NetworkImage('https://picsum.photos/id/${imagesIds[index]}500/300'),
+            );
+          }
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(onPressed: ()=> context.pop(), child: const Icon(Icons.arrow_back_ios_new_outlined),),
+    );
   }
 }
